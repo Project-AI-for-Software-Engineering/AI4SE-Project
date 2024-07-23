17c14242c55ac7c012bef667975caad7433fbfa1
nruip2 <n.ruizp2@uniandes.edu.co>
2024-07-23 10:14:55 -0500

    routing

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import ListEvents from "./components/ListEvents"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div classname="app">
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route path="/ListEvents/:sport" element={< ListEvents />}/> */}
+        </Routes>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
