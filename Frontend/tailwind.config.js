ad32815f221a0755a3fa49f7c6a357ef50e8bea3
Sebastian Gaona <sebastiangaona@MacBook-Air-de-Sebastian.local>
2024-07-23 12:07:38 -0500

    update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
   prefix: "",
   theme: {
@@ -18,14 +18,6 @@ module.exports = {
     },
     extend: {
       colors: {
-        "gsi-one": "#c1e871",
-        "gsi-two": "#04cbfa",
-        "gsi-three": "#041c3c",
-        "gsi-four": "#5f727e",
-        "gsi-five": "#0480ad",
-        "gsi-six": "#848cgc",
-        "gsi-seven": "#0e6581",
-        "gsi-eight": "#2f465a",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -74,17 +66,12 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "caret-blink": {
-          "0%,70%,100%": { opacity: "1" },
-          "20%,50%": { opacity: "0" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
