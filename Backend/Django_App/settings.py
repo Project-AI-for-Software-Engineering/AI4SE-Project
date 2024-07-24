ebaa37f16e3d6a8a887f27679333ee7ccc740361
nruip2 <n.ruizp2@uniandes.edu.co>
2024-07-24 18:47:55 -0500

    db config

--- Backend/Django_App/settings.py
+++ Backend/Django_App/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(9n6&)tx4r*k=#&x#t7q*sv90y)yu=z7n2!b_zsn4-towo=d@h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Django_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AISE_db',
+        'USER': 'admin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
