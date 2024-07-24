4dc61fb9ad3f9d51b0f26fc6d46141014e67cdfd
nruip2 <n.ruizp2@uniandes.edu.co>
2024-07-23 20:40:32 -0500

    List Events

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import ListEvents from "./components/ListEvents"
+import ListEvents from "./components/ListEvents"
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div classname="app">
+      <div>
         <Routes>
-          {/* <Route path="/" element={<Navigate to="/home"/>}/>
-          <Route path="/ListEvents/:sport" element={< ListEvents />}/> */}
+          <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route path="/ListEvents/" element={< ListEvents />}/>
         </Routes>
       </div>
 
