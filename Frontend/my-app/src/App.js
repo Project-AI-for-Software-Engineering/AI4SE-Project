584fa3f3d1f4aed038cb42d204c30262f94bac72
nruip2 <n.ruizp2@uniandes.edu.co>
2024-07-24 09:46:32 -0500

    front fix

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ListEvents from "./components/ListEvents"
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route path="/ListEvents/" element={< ListEvents />}/>
+        </Routes>
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
