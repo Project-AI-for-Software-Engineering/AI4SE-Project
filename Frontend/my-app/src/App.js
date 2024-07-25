3ad63896e57e2a444af91a760defe6280c6b8f26
Sebastian Gaona <sebastiangaona@MacBook-Air-de-Sebastian.local>
2024-07-25 11:33:21 -0500

    Wallets 2.0

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home" ; 
 import ListEvents from "./components/ListEvents";
+import Wallets from "./components/Wallets";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/ListEvents" element={<ListEvents />} />
+          <Route path="/Wallets" element={<Wallets />} />
         </Routes>
       </div>
     </BrowserRouter>
