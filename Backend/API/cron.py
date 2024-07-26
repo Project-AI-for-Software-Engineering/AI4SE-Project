d38a3f5cd3274561382946e5e2e660db3a7228b1
Jose David Florez Ruiz <jd.florezr1@uniandes.edu.co>
2024-07-26 06:37:56 -0500

    Fix Cronjob

--- Backend/API/cron.py
+++ Backend/API/cron.py
@@ -2,10 +2,10 @@ from django_cron import CronJobBase, Schedule
 from .views import send_mails
 
 class SendMailsCronJob(CronJobBase):
-    RUN_EVERY_MINS = 1
+    RUN_EVERY_MINS = 15
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'API.send_mails_cron_job'  # a unique code
+    code = 'API.send_mails_cron_job'
 
     def do(self):
         send_mails(None)
